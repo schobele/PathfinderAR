f5b92f201ec5af6176c704b78f23025f7b9bb747
Leo Schoberwalter <schobele@leos-imac.local>
2018-05-03 14:49:23 +0200

    Initial Commit

---
+++ prototype_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prototype_2
+//
+//  Created by Leo Schoberwalter on 03.05.18.
+//  Copyright © 2018 Leo Schoberwalter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
